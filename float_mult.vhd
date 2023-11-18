e087e84830b30538efb7a1dc7a0dda03b5c43041
GrnRant <gn.rant@gmail.com>
2023-11-18 09:16:05 -0300

    Multiplicador de punto flotante corregido

--- float_mult.vhd
+++ float_mult.vhd
@@ -23,12 +23,13 @@ architecture float_mult_arch of float_mult is
     --Bits de exponente de cada uno de los registros
     signal ex : unsigned(NE - 1 downto 0); --Exponente de x_i
     signal ey : unsigned(NE - 1 downto 0); --Exponente de y_i
-    signal ep1 : unsigned(NE downto 0) := (others => '0'); --Exp parcial obtenido a partir de ex y ey
-    signal ep2 : unsigned(NE downto 0) := (others => '0'); --Exp parcial usado para decidir si es cero el resultado
+    signal ep : integer; --Exp parcial obtenido a partir de ex y ey
+    signal er_int : integer := 0; --Exp resultante como entero
     signal er : unsigned(NE - 1 downto 0); --Exponente resultante si no  hay saturación
     signal emax : unsigned(NE - 1 downto 0); --Exponente resultante si hay saturación
+    signal emin : unsigned(NE - 1 downto 0); --Exponente resultante si hay saturación
     signal ezero : unsigned(NE - 1 downto 0); --Constante usada cuando resultado es cero
-    constant esat : unsigned(NE downto 0) := ('0', others => '1');
+    constant esat : unsigned(NE - 1 downto 0) := ('0', others => '1');
 
     --Bits de mantisa de cada uno de los registros
     signal mx : unsigned(NF - 1 downto 0); --Mantisa de cada una de x_i
@@ -46,42 +47,46 @@ architecture float_mult_arch of float_mult is
 
 begin
     --Inicialización exponente saturación, de mantisas, exponentes y bits de signo de entrada
-    emax <= (others => '1');
-    emax(0) <= '0';
     sx <= x_i(NF + NE);
     sy <= y_i(NF + NE);
-    ex <= unsigned(x_i((NE + NF) - 1 downto NF));
-    ey <= unsigned(y_i((NE + NF) - 1 downto NF));
+
     emax(NE - 1 downto 1) <= (others => '1');
     emax(0) <= '0';
+    emin(NE - 1 downto 0) <= to_unsigned(1, NE);
+    ex <= unsigned(x_i((NE + NF) - 1 downto NF));
+    ey <= unsigned(y_i((NE + NF) - 1 downto NF));
     ezero <= (others => '0');
+
     mx <= unsigned(x_i(NF - 1 downto 0));
     my <= unsigned(y_i(NF - 1 downto 0));
     mmax <= (others => '1');
     mzero <= (others => '0');
 
-    --Operaciones para el signo
-    sr <= sx xor sy;
-
-    --Operaciones para la mantisa
+    --Operaciones parciales para la mantisa
     mp1 <= ('1' & mx) * ('1' & my);
-    ov <= mp1(2 * NF + 1); --Overflow = al bit más significativo
-    mp2 <= mp1(2 * NF downto NF + 1) when ov = '1' else
-        mp1(2 * NF - 1 downto NF); --Se considera ov y se saca 1 implícito
-    mr <= mzero when ((mx = 0 and ex = 0) or (my = 0 and ey = 0)) else mp2;
+    --Overflow = al bit más significativo
+    ov <= mp1(2*NF + 1); 
 
-    --Operaciones para el exponente
-    ep1 <= ('0' & ex) + ('0' & ey) - ('0' & exc);
-    ep2 <= ep1 when ov = '0' else ep1 + 1; --Se suma el overflow si es que hay
-    er <= ezero when ((mx = 0 and ex = 0) or (my = 0 and ey = 0)) else
-     ep2(NE - 1 downto 0); --Si mx = 0 o my = 0 entonces exponente = 0
+    --Resultados parciales
+    mp2 <= mp1(2 * NF downto NF + 1) when mp1(2*NF + 1) = '1' 
+            else mp1(2 * NF - 1 downto NF); --Normalización de mantisa
+    ep <= (to_integer(ex) + to_integer(ey) - to_integer(exc) + 1) when mp1(2*NF + 1) = '1'
+            else to_integer(ex) + to_integer(ey) - to_integer(exc); --Resultado parcial exponente
 
-    --Verificación de saturación
-    --Si el exponente supera emax (valor máximo) entonces hay saturación
-    sat <= '1' when (ep2 >= esat) else '0';
+    --Resultados finales
+    --Operaciones para el signo
+    sr <= '0' when (ep < to_integer(emin)) or (mx = 0 and ex = 0) or (my = 0 and ey = 0) else sx xor sy;
+    --Resultado exponente
+    er_int <= 62 when ep > to_integer(emax) 
+                else to_integer(ezero) when (ep < to_integer(emin)) or (mx = 0 and ex = 0) or (my = 0 and ey = 0) 
+                else ep;
+    er <= to_unsigned(er_int, NE);
+    --Resultado mantisa
+    mr <= mmax when ep > to_integer(emax) 
+            else mzero when (ep < to_integer(emin)) or (mx = 0 and ex = 0) or (my = 0 and ey = 0) 
+            else mp2;
 
     --Salida del resultado final (concatenación de signo, exponente, mantisa resultantes)
-    res_o <= std_logic_vector(sr & er & mr) when sat = '0' else
-        std_logic_vector(sr & emax & mmax);
+    res_o <= std_logic_vector(sr & er & mr);
 
 end float_mult_arch;
